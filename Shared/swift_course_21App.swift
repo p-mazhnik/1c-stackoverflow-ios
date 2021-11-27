151e5c9e2d3ec73c6e83b2dfd70c59e092a9ca09
p-mazhnik <pavel.mazhnik@gmail.com>
2021-11-28 00:45:31 +0300

    Initial Commit

---
+++ Shared/swift_course_21App.swift
@@ -0,0 +1,17 @@
+//
+//  swift_course_21App.swift
+//  Shared
+//
+//  Created by Pavel Mazhnik on 28.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct swift_course_21App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
